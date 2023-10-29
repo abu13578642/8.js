e70fa901ed24ff7f0167f782373b95038089230e
abu13578642 <holololl135@gmail.com>
2023-10-29 11:35:34 +0300

    salom

---
+++ 1.js
@@ -0,0 +1,7 @@
+let loader = document.querySelector(".line")
+let c = document.querySelector(".container")
+
+window.onload = () => {
+    loader.setAttribute("style", "display : none")
+    c.style.dislpay = "block"
+}
